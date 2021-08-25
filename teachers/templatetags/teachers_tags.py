ee78733b16883937a081b0ac77eed9b6dc00662c
Taras265 <70756830+Taras265@users.noreply.github.com>
2021-08-25 11:28:31 +0300

    Update teachers_tags.py

--- teachers/templatetags/teachers_tags.py
+++ teachers/templatetags/teachers_tags.py
@@ -25,8 +25,11 @@ def mark_filter(date, student):
 @register.simple_tag
 def get_simple_mark(date, topic, student):
     simple_mark = MarkType.objects.get(pk=2)
-    mark = Mark.objects.get(topic=topic, student=student.id, date=date, type=simple_mark)
-    return mark.mark
+    #mark = Mark.objects.get(topic=topic, student=student.id, date=date, type=simple_mark)
+    #return mark.mark
+    mark = Mark.objects.filter(
+        topic=topic, student=student.id, date=date, type=simple_mark)
+    return mark
 
 
 @register.filter
